79ef6154cb1ea1f6fd02df91b80d0a8b552e6fb8
Zack <zack1444@gmail.com>
2019-01-17 15:44:30 -0800

    fixing jumbotron

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/scraper_controller.js");
+var routes = require("./controllers/scraperController.js");
 
 app.use("/", routes);
 
